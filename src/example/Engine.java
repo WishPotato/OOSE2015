cf64475f105fdacf5781d3b9f1006013b7627056
WishPotato <wish92jfn@hotmail.com>
2015-03-16 14:28:26 +0100

    Edited Engine.java (still work in progress)

--- src/example/Engine.java
+++ src/example/Engine.java
@@ -1,12 +1,11 @@
 package example;
 
 import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class Engine extends BasicGame{
+public class Engine extends StateBasedGame{
 
 	public Engine() {
 		super("Gauntlet");
@@ -20,31 +19,18 @@ public class Engine extends BasicGame{
 			game.setDisplayMode(Window.WIDTH, Window.HEIGHT, false);
 			game.start();
 		} catch (SlickException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 	
 	@Override
-	public void init(GameContainer gc) throws SlickException {
+	public void initStatesList(GameContainer gc) throws SlickException {
 		gc.setTargetFrameRate(60);
 		gc.setAlwaysRender(true); // Renders even if the game is minimized
 		gc.setMaximumLogicUpdateInterval(60);
 		gc.setVSync(true);
 		gc.setShowFPS(false); // Removes the FPS counter
-	}
-
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void update(GameContainer gc, int delta) throws SlickException {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
